98520b19e37e5a964c06c32b5984b61f8b2f3669
jettFeng <1130034920@>
2018-07-08 15:06:20 +0800

    提交3-3

--- app/src/App.js
+++ app/src/App.js
@@ -6,15 +6,28 @@ class App extends Component {
     return (
       <div>
         <h2>团长:{boss}</h2>
-        <One boss='张大喵' />
+        <One boss="张大喵" />
       </div>
     );
   }
 }
 
 class One extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      solders: ['虎子', '柱子', '五根生'],
+    };
+  }
   render () {
-    return <h2>营长：{this.props.boss}</h2>;
+    return (
+      <div>
+        <h2>营长：{this.props.boss}</h2>
+        <ul>
+          {this.state.solders.map(v=><li key={v}>{v}</li>)}
+        </ul>
+      </div>
+    );
   }
 }
 
