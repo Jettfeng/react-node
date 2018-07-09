c4c60c9f6264afa27fd0fdbddf4d2879bd950fa0
jettFeng <1130034920@>
2018-07-09 22:33:08 +0800

    3-7 antd-mobile 组件使用

--- app/Explain.md
+++ app/Explain.md
@@ -1,10 +1,38 @@
-<h3>3-5 React基础知识回顾5-React生命周期</h3>
-<p>初始化周期:componentWillMount</p>
-<p>组件重新渲染生命周期:componentWillUpdate</p>
-<p>组件卸载声明周期:componentWillUnmount</p>
+<h3>3-7 antd-mobile 组件使用</h3>
+<p>1、安装： npm install antd-mobile --save</p>
+<p>2、配置按需加载：(安装插件：npm install babel-plugin-import --save-dev)</p>
+<p>3、修改package.json配置文件,babel项</p>
+<p>"babel": {</p>
+<p>"presets": [</p>
+<p>"react-app"</p>
+<p> ],</p>
+<p> "plugins": [</p>
+<p>[</p>
+<p> "import",</p>
+<p> {</p>
+<p> "libraryName": "antd-mobile",</p>
+<p> "style": "css"</p>
+<p>}</p>
+<p>]</p>
+<p> ]</p>
+<p>}</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
+    
+      
+   
+   
+      
+       
+       
+         
+         
+        
+      
+   
+  
+
 
 
--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Button, List} from 'antd-mobile';
 class App extends Component {
   render () {
     const boss = '李云龙';
@@ -17,29 +17,34 @@ class One extends Component {
     super (props);
     this.state = {
       solders: ['虎子', '柱子', '五根生'],
-    }
-    this.addSolder = this.addSolder.bind(this)
+      listHeader:'表头',
+      listFooter:'表脚'
+    };
+    this.addSolder = this.addSolder.bind (this);
   }
-  componentWillMount(){
-    console.log('组件马上就要加载了');
+  componentWillMount () {
+    console.log ('组件马上就要加载了');
   }
-  componentDidMount(){
-    console.log('组件加载完毕');
+  componentDidMount () {
+    console.log ('组件加载完毕');
   }
-  addSolder(){
-      this.setState({
-        solders:[...this.state.solders,'新添加的'+Math.random()]
-      })
+  addSolder () {
+    this.setState ({
+      solders: [...this.state.solders, '新添加的' + Math.random ()]
+    });
   }
   render () {
-    console.log('组件正在加载');
+    console.log ('组件正在加载');
     return (
       <div>
         <h2>营长：{this.props.boss}</h2>
-        <button onClick={this.addSolder.bind(this)}>添加</button>
-        <ul>
+        <Button type="primary" onClick={this.addSolder.bind (this)}>添加</Button>
+        <List renderHeader={()=>this.state.listHeader} renderFooter={()=>this.state.listFooter}>
+          {this.state.solders.map (v => <List.Item key={v}>{v}</List.Item>)}
+        </List>
+        {/* <ul>
           {this.state.solders.map(v=><li key={v}>{v}</li>)}
-        </ul>
+        </ul> */}
       </div>
     );
   }
