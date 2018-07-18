09932ae2b1b12823538144e87b062b3a4331697f
jettFeng <1130034920@>
2018-07-18 23:18:20 +0800

    6-7 登录注册-数据库模型建立

--- app/server/user.js
+++ app/server/user.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const Router = express.Router()
-
+const modle = require('./model')
+const User = modle.getModel('user')
+Router.get('/list',function(req,res){
+  User.find({},function(err,doc){
+    return res.json(doc)
+  })
+})
 Router.get('/info',function(req,res){
     // 用户有没有cookie
   return res.json({code:1})
