22ae6e5f4a1dce12b4c51bb6277f0718a738debf
jettFeng <1130034920@>
2018-07-24 22:48:55 +0800

    7-2 完善信息-boss 信息完善页面后端

--- app/src/util.js
+++ app/src/util.js
@@ -1,10 +1,10 @@
-export function getRdirectPath({type,avatar}){
-    // 根据用户信息跳转页面地址
-    // user.type/boss/genius
-    // user.avatar /bossinfo/genuisinfo
-    let url = (type==='boss')?'/boss':'/genius'
-    if(!avatar){
-        url+= 'info'
-    }
-    return url
+export function getRedirectPath({type, avatar}) {
+  // 根据用户信息 返回跳转地址
+  // user.type /boss /genius
+  // user.avatar /bossinfo /geniusinfo
+  let url = type === 'boss' ? '/boss' : '/genius';
+  if (!avatar) {
+    url += 'info';
+  }
+  return url;
 }
