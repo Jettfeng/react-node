2f2131304a54efbea1a546b441c480364ab4e033
jettFeng <1130034920@>
2018-07-27 01:10:39 +0800

    8-2 牛人列表-导航和跳转

--- app/src/component/dashboard/dashboard.js
+++ app/src/component/dashboard/dashboard.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {NavBar} from 'antd-mobile';
+import NavLinkBar from '../navlinkbar/navlinkbar'
 import {Route} from 'react-router-dom';
 
+function Boss () {
+  return <div>Boss</div>;
+}
+function Genius () {
+  return <div>Genius</div>;
+}
+function Msg () {
+  return <div>Msg</div>;
+}
+function User () {
+  return <div>User</div>;
+}
+@connect (state => state)
 class Dashboard extends React.Component {
   render () {
+    console.log (this.props);
+    const pathname = this.props.location.pathname;
+    const user = this.props.user;
+    const navList = [
+        {
+            path:'/boss',
+            text:'牛人',
+            icon:'boss',
+            title:'牛人列表',
+            component:Boss,
+            hide:user.type=='genius'
+        },
+        {
+            path:'/genius',
+            text:'boss',
+            icon:'job',
+            title:'BOSS列表',
+            component:Genius,
+            hide:user.type=='boss'
+        },
+        {
+            path:'/msg',
+            text:'消息',
+            icon:'msg',
+            title:'消息列表',
+            component:Msg
+        },
+        {
+            path:'/me',
+            text:'我',
+            icon:'user',
+            title:'个人中心',
+            component:User
+        }
+    ]
     return (
       <div>
-        <div>header</div>
-        
-        <div>footer</div>
+        <NavBar mode="dard">
+          {navList.find (v => v.path == pathname).title}
+        </NavBar>
+        <h2>content</h2>
+        <NavLinkBar data={navList}></NavLinkBar>
       </div>
     );
   }
