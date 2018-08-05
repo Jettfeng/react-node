c693c1d271e504b2bd2c4b64ef7fc08ad1070465
jettFeng <1130034920@>
2018-08-05 22:09:53 +0800

    10-2 socket.io前后端联通

--- app/server/server.js
+++ app/server/server.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser') 
-const userRouter = require('./user')
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const cookieParser = require ('cookie-parser');
 // 新建app
-const app = express()
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use('/user',userRouter)
+const app = express ();
+// work with express
+const server = require ('http').Server (app);
+const io = require ('socket.io') (server);
+// 建立连接
+io.on ('connection', function (socket) {
+  console.log ('user login');
+  console.log ('a user connected');
+});
 
-app.listen(9093,function(){
-    console.log('node app start at port 9093');
-})
+const userRouter = require ('./user');
+
+app.use (cookieParser ());
+app.use (bodyParser.json ());
+app.use ('/user', userRouter);
+
+server.listen (9093, function () {
+  console.log ('node app start at port 9093');
+});
--- app/src/component/chat/chat.js
+++ app/src/component/chat/chat.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import io from 'socket.io-client';
 class Chat extends React.Component {
   constructor (props) {
     super (props);
   }
   componentDidMount () {
-    console.log (this.props);
+    //   建立连接
+    const socket = io ('ws://localhost:9093');
   }
   render () {
     return <div>chat with user:{this.props.match.params.user}</div>;
