5b418285ed2074ebdf4ba4464c69eac0ae31d072
jettFeng <1130034920@>
2018-08-14 23:07:21 +0800

    11-2 聊天列表展示

--- app/src/component/msg/msg.js
+++ app/src/component/msg/msg.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {List} from 'antd-mobile';
 
 @connect (state => state)
 class Msg extends React.Component {
-  componentDidMount () {
-    console.log (this.props);
+  getLast (arr) {
+    return arr[arr.length - 1];
   }
   render () {
+    const Item = List.Item;
+    const Brief = Item.Brief;
+    const userid = this.props.user._id;
+    const userInfo = this.props.chat.users;
     const msgGroup = {};
     this.props.chat.chatmsg.forEach (v => {
       msgGroup[v.chatid] = msgGroup[v.chatid] || [];
       msgGroup[v.chatid].push (v);
     });
-    console.log(msgGroup);
-    
+    console.log (msgGroup);
+    const chatList = Object.values (msgGroup);
+    console.log (chatList);
     //按照聊天用户分组，根据chatid
-    return <div>Msg</div>;
+    return (
+      <div>
+        {chatList.map (v => {
+          const lastItem = this.getLast (v);
+          const targetId = (v[0].from = userid ? v[0].to : v[0].from);
+          if (!userInfo[targetId]) {
+            return null;
+          }
+          // const name = userInfo[targetId]? userInfo[targetId].name:''
+          // const avatar = userInfo[targetId]? userInfo[targetId].avatar:''
+          return (
+            <List key={lastItem._id}>
+              <Item thumb={require (`../img/${userInfo[targetId].avatar}.png`)}>
+                {lastItem.content}
+                <Brief>{userInfo[targetId].name}</Brief>
+              </Item>
+            </List>
+          );
+        })}
+      </div>
+    );
   }
 }
 
