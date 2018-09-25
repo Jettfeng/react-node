9944481ec07744959bac06148e1c5a034ad807f5
1130034920@qq.com <1130034920@qq.com>
2018-09-25 22:27:34 +0800

    提交

--- app/server/server.js
+++ app/server/server.js
@@ -6,38 +6,41 @@ mongoose.connect(DB_URL,{ useMongoClient: true })//如果不配置{ useMongoClie
 mongoose.connection.on('connected',function(){
     console.log('mongo connect success');
 })
+mongoose.connection.on('error',function(){
+    console.log('mongo connect fail');
+})
 // 类似于mysql的表， mongo里有文档、字段的概念
 const User = mongoose.model('user',new mongoose.Schema({
     user:{type:String,require:true},
     age:{type:Number,require:true}
 }))
 // 新增数据
-// User.create({
-//     user:'xiaohua',
-//     age:12
-// },function(err,doc){
-//     if(!err){
-//       console.log(doc);
-//     }else{
-//         console.log(err);
-//     }
-// })
-// 删除数据
-User.remove({age:18},function(err,doc){
-  if(!err){
-      console.log(doc);
-  }else{
-      console.log(err); 
-  }
-})
-// 更新数据
-User.update({'user':'xiaoming'},{'$set':{age:26}},function(err,doc){
+User.create({
+    user:'xiaoming',
+    age:12
+},function(err,doc){
     if(!err){
-        console.log(doc);
+      console.log(doc);
     }else{
         console.log(err);
     }
 })
+// 删除数据
+// User.remove({age:18},function(err,doc){
+//   if(!err){
+//       console.log(doc);
+//   }else{
+//       console.log(err); 
+//   }
+// })
+// 更新数据
+// User.update({'user':'xiaoming'},{'$set':{age:26}},function(err,doc){
+//     if(!err){
+//         console.log(doc);
+//     }else{
+//         console.log(err);
+//     }
+// })
 // 新建app
 const app = express()
 app.get('/',function(req,res){
