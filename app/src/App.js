1662c5f5b2a774f93fdc727195cc7f2a0522741e
1130034920@qq.com <1130034920@qq.com>
2018-09-28 20:11:35 +0800

    提价

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-  render () {
-    const boss = '李云龙';
+  render() {
+    const boss = "李云龙";
     return (
       <div>
-        <h2>团长:{boss}</h2>
+        <h2>团长: {boss}</h2>
         <One boss="张大喵" />
       </div>
     );
@@ -13,32 +13,37 @@ class App extends Component {
 }
 
 class One extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-      solders: ['虎子', '柱子', '五根生'],
-    }
-    this.addSolder = this.addSolder.bind(this)
+      solders: ["虎子", "柱子", "五根生"]
+    };
+    this.addSolder = this.addSolder.bind(this);
   }
-  componentWillMount(){
-    console.log('组件马上就要加载了');
+  componentWillMount() {
+    console.log("组件马上就要加载了");
   }
-  componentDidMount(){
-    console.log('组件加载完毕');
+  componentDidMount() {
+    console.log("组件加载完毕");
   }
-  addSolder(){
-      this.setState({
-        solders:[...this.state.solders,'新添加的'+Math.random()]
-      })
+  addSolder() {
+    this.setState({
+      solders: [...this.state.solders, "新添加的" + Math.random()]
+    });
   }
-  render () {
-    console.log('组件正在加载');
+  render() {
+    console.log("组件正在加载");
     return (
       <div>
-        <h2>营长：{this.props.boss}</h2>
+        <h2>
+          营长：
+          {this.props.boss}
+        </h2>
         <button onClick={this.addSolder.bind(this)}>添加</button>
         <ul>
-          {this.state.solders.map(v=><li key={v}>{v}</li>)}
+          {this.state.solders.map(v => (
+            <li key={v}>{v}</li>
+          ))}
         </ul>
       </div>
     );
