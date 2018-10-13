44104361139ad5ce3984d458139c20d0f468a1fa
1130034920@qq.com <1130034920@qq.com>
2018-10-13 21:58:41 +0800

    第二次提交

--- app/src/index.js
+++ app/src/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-import reducers from './reducer';
-import './config';
-import './index.css'
+import reducers from "./reducer";
+import "./config";
+import "./index.css";
 
-import Login from './container/login/login';
-import Register from './container/register/register';
-import AuthRoute from './component/authroute/authroute';
+import Login from "./container/login/login";
+import Register from "./container/register/register";
+import AuthRoute from "./component/authroute/authroute";
 
 // 配置redux-devtools
 const reduxDevtools = window.devToolsExtension
-  ? window.devToolsExtension ()
+  ? window.devToolsExtension()
   : f => f;
 
-const store = createStore (
+const store = createStore(
   reducers,
-  compose (applyMiddleware (thunk), reduxDevtools)
+  compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+  )
 ); //开启中间件
 
-function Boss(){
-    return <h2>这里是Boss页面</h2>
+function Boss() {
+  return <h2>这里是Boss页面</h2>;
 }
 
-ReactDOM.render (
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <div>
@@ -38,5 +41,5 @@ ReactDOM.render (
       </div>
     </BrowserRouter>
   </Provider>,
-  document.getElementById ('root')
+  document.getElementById("root")
 );
--- app/src/redux/user.redux.js
+++ app/src/redux/user.redux.js
@@ -1,46 +1,46 @@
-import axios from 'axios'
-const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-const ERROR_MSG = 'ERROR_MSG';
+import axios from "axios";
+const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+const ERROR_MSG = "ERROR_MSG";
 const initState = {
   isAuth: false,
-  msg: '',
-  user: '',
-  pwd: '',
-  type: '',
+  msg: "",
+  user: "",
+  pwd: "",
+  type: ""
 };
 // reducer
-export function user (state = initState, action) {
+export function user(state = initState, action) {
   switch (action.type) {
     case REGISTER_SUCCESS:
-      return {...state, msg: '', isAuth: true,...action.payload};
+      return { ...state, msg: "", isAuth: true, ...action.payload };
     case ERROR_MSG:
-      return {...state,isAuth:false,msg:action.msg}
+      return { ...state, isAuth: false, msg: action.msg };
     default:
       return state;
   }
 }
 
-function registerSuccess (data) {
-  return {type: REGISTER_SUCCESS, payload: data};
+function registerSuccess(data) {
+  return { type: REGISTER_SUCCESS, payload: data };
 }
 
-function errorMsg (msg) {
-  return {msg, type: ERROR_MSG};
+function errorMsg(msg) {
+  return { msg, type: ERROR_MSG };
 }
 
-export function register({user, pwd, repeatpwd, type}) {
+export function register({ user, pwd, repeatpwd, type }) {
   if (!user || !pwd || !type) {
-    return errorMsg ('用户名密码必须输入');
+    return errorMsg("用户名密码必须输入");
   }
   if (pwd != repeatpwd) {
-    return errorMsg ('密码和确认密码不同');
+    return errorMsg("密码和确认密码不同");
   }
   return dispatch => {
-    axios.post ('/user/register', {user, pwd, type}).then (res => {
+    axios.post("/user/register", { user, pwd, type }).then(res => {
       if (res.status == 200 && res.data.code == 0) {
-        dispatch (registerSuccess ({user, pwd, type}));
+        dispatch(registerSuccess({ user, pwd, type }));
       } else {
-        dispatch (errorMsg (res.data.msg));
+        dispatch(errorMsg(res.data.msg));
       }
     });
   };
