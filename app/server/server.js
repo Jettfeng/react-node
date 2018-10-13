34e208a480617ad4b5fbcbb81500c86e9fce060c
1130034920@qq.com <1130034920@qq.com>
2018-10-13 23:08:31 +0800

    提交

--- app/server/server.js
+++ app/server/server.js
@@ -5,7 +5,7 @@ const userRouter = require('./user')
 // 新建app
 const app = express()
 app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(bodyParser.json())//解析post请求体
 app.use('/user',userRouter)
 
 app.listen(9093,function(){
