20662ff434b5ba8fa2e4d1c18ec4c578975ae6e5
1130034920@qq.com <1130034920@qq.com>
2018-10-15 22:17:35 +0800

    第二次提交

--- app/server/server.js
+++ app/server/server.js
@@ -7,7 +7,9 @@ const app = express()
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user',userRouter)
-
+app.get('/',function(req,res){
+    res.json({code:0})
+})
 app.listen(9093,function(){
     console.log('node app start at port 9093');
 })
