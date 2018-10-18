260c06acdcb5920b25786cd6a39c31f4e2ac2408
1130034920@qq.com <1130034920@qq.com>
2018-10-18 22:53:55 +0800

    6-11

--- app/server/user.js
+++ app/server/user.js
@@ -1,49 +1,48 @@
-const express = require('express')
-const utils = require('utility')
-const Router = express.Router()
-const modle = require('./model')
-const User = modle.getModel('user')
-Router.get('/list',function(req,res){
+const express = require("express");
+const utils = require("utility");
+const Router = express.Router();
+const modle = require("./model");
+const User = modle.getModel("user");
+Router.get("/list", function(req, res) {
   // User.remove({},function(e,d){})
-  User.find({},function(err,doc){
-    return res.json(doc)
-  })
-})
-Router.post('/login',function(req,res){
-  const {user,pwd} = req.body
-  User.findOne({user,pwd:md5Pwd(pwd)},{'pwd':0},function(err,doc){//{'pwd':0}表示返回的doc里面不显示pwd字段
-    if(!doc){
-      return res.json({code:1,msg:'用户名不存在或者密码错误'})
+  User.find({}, function(err, doc) {
+    return res.json(doc);
+  });
+});
+Router.post("/login", function(req, res) {
+  const { user, pwd } = req.body;
+  User.findOne({ user, pwd: md5Pwd(pwd) }, { pwd: 0 }, function(err, doc) {
+    //{'pwd':0}表示返回的doc里面不显示pwd字段
+    if (!doc) {
+      return res.json({ code: 1, msg: "用户名不存在或者密码错误" });
     }
-    return res.json({code:0,data:doc})
-  })
-})
-Router.post('/register',function(req,res){
-  const {user,pwd,type} = req.body
-  User.findOne({user},function(err,doc){
-    console.log(doc)
-    if(doc){
-      return res.json({code:1,msg:'用户名重复'})
+    return res.json({ code: 0, data: doc });
+  });
+});
+Router.post("/register", function(req, res) {
+  const { user, pwd, type } = req.body;
+  User.findOne({ user }, function(err, doc) {
+    console.log(doc);
+    if (doc) {
+      return res.json({ code: 1, msg: "用户名重复" });
     }
-    User.create({user,type,pwd:md5Pwd(pwd)},function(err,doc){
-      if(err){
-        return res.json({code:1,msg:'后端出错了'})
+    User.create({ user, type, pwd: md5Pwd(pwd) }, function(err, doc) {
+      if (err) {
+        return res.json({ code: 1, msg: "后端出错了" });
       }
-      
-      return res.json({code:0})
-    })
-  })
-})
-Router.get('/info',function(req,res){
-    // 用户有没有cookie
-  return res.json({code:1})
-})
 
+      return res.json({ code: 0 });
+    });
+  });
+});
+Router.get("/info", function(req, res) {
+  // 用户有没有cookie
+  return res.json({ code: 1 });
+});
 
-function md5Pwd(pwd){
-	const salt = 'imooc_is_good_3957x8yza6!@#IUHJh~~'
-	return utils.md5(utils.md5(pwd+salt))
+function md5Pwd(pwd) {
+  const salt = "imooc_is_good_3957x8yza6!@#IUHJh~~";
+  return utils.md5(utils.md5(pwd + salt));
 }
 
-
-module.exports = Router
+module.exports = Router;
