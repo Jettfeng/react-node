52e4d300a5b070fa05edd9fbce332ca3f9060085
1130034920@qq.com <1130034920@qq.com>
2018-10-21 17:12:28 +0800

    房贷V型

--- app/src/component/dashboard/dashboard.js
+++ app/src/component/dashboard/dashboard.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {NavBar} from 'antd-mobile';
-import NavLinkBar from '../navlinkbar/navlinkbar'
-import {Route} from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { NavBar } from "antd-mobile";
+import NavLinkBar from "../navlinkbar/navlinkbar";
+import { Route, Switch } from "react-router-dom";
 
-function Boss () {
+function Boss() {
   return <div>Boss</div>;
 }
-function Genius () {
+function Genius() {
   return <div>Genius</div>;
 }
-function Msg () {
+function Msg() {
   return <div>Msg</div>;
 }
-function User () {
+function User() {
   return <div>User</div>;
 }
-@connect (state => state)
+@connect(state => state)
 class Dashboard extends React.Component {
-  render () {
-    console.log (this.props);
+  render() {
+    console.log(this.props);
     const pathname = this.props.location.pathname;
     const user = this.props.user;
     const navList = [
-        {
-            path:'/boss',
-            text:'牛人',
-            icon:'boss',
-            title:'牛人列表',
-            component:Boss,
-            hide:user.type=='genius'
-        },
-        {
-            path:'/genius',
-            text:'boss',
-            icon:'job',
-            title:'BOSS列表',
-            component:Genius,
-            hide:user.type=='boss'
-        },
-        {
-            path:'/msg',
-            text:'消息',
-            icon:'msg',
-            title:'消息列表',
-            component:Msg
-        },
-        {
-            path:'/me',
-            text:'我',
-            icon:'user',
-            title:'个人中心',
-            component:User
-        }
-    ]
+      {
+        path: "/boss",
+        text: "牛人",
+        icon: "boss",
+        title: "牛人列表",
+        component: Boss,
+        hide: user.type == "genius"
+      },
+      {
+        path: "/genius",
+        text: "boss",
+        icon: "job",
+        title: "BOSS列表",
+        component: Genius,
+        hide: user.type == "boss"
+      },
+      {
+        path: "/msg",
+        text: "消息",
+        icon: "msg",
+        title: "消息列表",
+        component: Msg
+      },
+      {
+        path: "/me",
+        text: "我",
+        icon: "user",
+        title: "个人中心",
+        component: User
+      }
+    ];
     return (
       <div>
         <NavBar mode="dard">
-          {navList.find (v => v.path == pathname).title}
+          {navList.find(v => v.path == pathname).title}
         </NavBar>
         <h2>content</h2>
-        <NavLinkBar data={navList}></NavLinkBar>
+        <NavLinkBar data={navList} />
       </div>
     );
   }
