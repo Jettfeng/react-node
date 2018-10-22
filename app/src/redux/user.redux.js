8580d5d0d532ce1f541102e877cfd669d5a3c7ce
1130034920@qq.com <1130034920@qq.com>
2018-10-22 23:12:21 +0800

    提交9-5

--- app/src/redux/user.redux.js
+++ app/src/redux/user.redux.js
@@ -1,90 +1,94 @@
-import axios from 'axios';
-import {getRedirectPath} from '../util';
-const ERROR_MSG = 'ERROR_MSG';
-const AUTH_SUCCESS = 'AUTH_SUCCESS';
-const LOGOUT = 'LOGOUT'
-const LOAD_DATA = 'LOAD_DATA';
+import axios from "axios";
+import { getRedirectPath } from "../util";
+const ERROR_MSG = "ERROR_MSG";
+const AUTH_SUCCESS = "AUTH_SUCCESS";
+const LOGOUT = "LOGOUT";
+const LOAD_DATA = "LOAD_DATA";
 const initState = {
-  redirectTo: '',
+  redirectTo: "",
   // isAuth: false,
-  msg: '',
-  user: '',
-  type: '',
+  msg: "",
+  user: "",
+  type: ""
 };
 // reducer
-export function user(state=initState, action){
-	switch(action.type){
-		case AUTH_SUCCESS:
-			return {...state, msg:'',redirectTo:getRedirectPath(action.payload),...action.payload,pwd:''}
-		case LOAD_DATA:
-			return {...state, ...action.payload}
-		case ERROR_MSG:
-			return {...state, isAuth:false, msg:action.msg}
-		case LOGOUT:
-			return {...initState,redirectTo:'/login'}
-		default:
-			return state
-	}
+export function user(state = initState, action) {
+  switch (action.type) {
+    case AUTH_SUCCESS:
+      return {
+        ...state,
+        msg: "",
+        redirectTo: getRedirectPath(action.payload),
+        ...action.payload,
+        pwd: ""
+      };
+    case LOAD_DATA:
+      return { ...state, ...action.payload };
+    case ERROR_MSG:
+      return { ...state, isAuth: false, msg: action.msg };
+    case LOGOUT:
+      return { ...initState, redirectTo: "/login" };
+    default:
+      return state;
+  }
 }
-function authSuccess (obj) {
+function authSuccess(obj) {
   // console.log(data)
-  const {pwd,...data} = obj 
-  return {type: AUTH_SUCCESS, payload: data};
+  const { pwd, ...data } = obj;
+  return { type: AUTH_SUCCESS, payload: data };
 }
 
-function errorMsg (msg) {
-  return {msg, type: ERROR_MSG};
+function errorMsg(msg) {
+  return { msg, type: ERROR_MSG };
 }
 
-export function loadData (userinfo) {
-  return {type: LOAD_DATA, payload: userinfo};
+export function loadData(userinfo) {
+  return { type: LOAD_DATA, payload: userinfo };
 }
-export function logoutSubmit(){
-  return {type:LOGOUT}
+export function logoutSubmit() {
+  return { type: LOGOUT };
 }
-export function update (data) {
+export function update(data) {
   return dispatch => {
-    axios.post ('/user/update', data).then (res => {
+    axios.post("/user/update", data).then(res => {
       if (res.status === 200 && res.data.code === 0) {
-        console.log(res)
-        dispatch (authSuccess (res.data.data));
+        console.log(res);
+        dispatch(authSuccess(res.data.data));
       } else {
-        dispatch (errorMsg (res.data.msg));
+        dispatch(errorMsg(res.data.msg));
       }
     });
   };
 }
-export function login({user, pwd}) {
+export function login({ user, pwd }) {
   if (!user || !pwd) {
-    return errorMsg ('用户名密码必须输入');
+    return errorMsg("用户名密码必须输入");
   }
   return dispatch => {
-    axios.post ('/user/login', {user, pwd}).then (res => {
+    axios.post("/user/login", { user, pwd }).then(res => {
       if (res.status === 200 && res.data.code === 0) {
-        dispatch (authSuccess (res.data.data));
+        dispatch(authSuccess(res.data.data));
       } else {
-        dispatch (errorMsg (res.data.msg));
+        dispatch(errorMsg(res.data.msg));
       }
     });
   };
 }
 
-export function register({user, pwd, repeatpwd, type}) {
+export function register({ user, pwd, repeatpwd, type }) {
   if (!user || !pwd || !type) {
-    return errorMsg ('用户名密码必须输入');
+    return errorMsg("用户名密码必须输入");
   }
   if (pwd !== repeatpwd) {
-    return errorMsg ('密码和确认密码不同');
+    return errorMsg("密码和确认密码不同");
   }
   return dispatch => {
-    axios.post ('/user/register', {user, pwd, type}).then (res => {
+    axios.post("/user/register", { user, pwd, type }).then(res => {
       if (res.status === 200 && res.data.code === 0) {
-        dispatch (authSuccess ({user, pwd, type}));
+        dispatch(authSuccess({ user, pwd, type }));
       } else {
-        dispatch (errorMsg (res.data.msg));
+        dispatch(errorMsg(res.data.msg));
       }
     });
   };
 }
-
-
