0a071cb89d2ddbf79c8de9fd14ec38b4e98bf667
1130034920@qq.com <1130034920@qq.com>
2018-10-23 22:45:25 +0800

    提交---高阶组件

--- app/src/component/imooc-form/imooc-form.js
+++ app/src/component/imooc-form/imooc-form.js
@@ -1,20 +1,19 @@
-import React from 'react';
-export default function imoocFrom (Comp) {
+import React from "react";
+export default function imoocFrom(Comp) {
   return class WrapperComp extends React.Component {
-    constructor (props) {
-      super (props);
+    constructor(props) {
+      super(props);
       this.state = {};
-      this.handleChange = this.handleChange.bind (this);
+      this.handleChange = this.handleChange.bind(this);
     }
-    handleChange (key, val) {
-        console.log(`key=${key};val=${val}`);
-        
-      this.setState ({
-        [key]: val,
+    handleChange(key, val) {
+      console.log(`key=${key};val=${val}`);
+      this.setState({
+        [key]: val
       });
     }
 
-    render () {
+    render() {
       return (
         <Comp
           handleChange={this.handleChange}
