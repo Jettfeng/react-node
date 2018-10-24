6cd0cd1bb95db03fefe357281eb1eaec19f85f2d
1130034920@qq.com <1130034920@qq.com>
2018-10-25 00:13:06 +0800

    聊天功能的实现--下

--- app/src/component/chat/chat.js
+++ app/src/component/chat/chat.js
@@ -1,48 +1,49 @@
-import React from 'react';
-import {List, InputItem, NavBar} from 'antd-mobile';
+import React from "react";
+import { List, InputItem, NavBar } from "antd-mobile";
 // import io from 'socket.io-client';
-import {connect} from 'react-redux';
-import {getMsgList,sendMsg,recvMsg} from '../../redux/chat.redux';
+import { connect } from "react-redux";
+import { getMsgList, sendMsg, recvMsg } from "../../redux/chat.redux";
 //   建立连接
 // const socket = io ('ws://localhost:9093');
-@connect (state => state, {getMsgList,sendMsg,recvMsg})
+@connect(
+  state => state,
+  { getMsgList, sendMsg, recvMsg }
+)
 class Chat extends React.Component {
-  constructor (props) {
-    super (props);
-    this.state = {text: '', msg: []};
+  constructor(props) {
+    super(props);
+    this.state = { text: "", msg: [] };
   }
-  componentDidMount () {
-    this.props.getMsgList()
-    this.props.recvMsg()
+  componentDidMount() {
+    this.props.getMsgList();
+    this.props.recvMsg();
   }
-  handleSubmit () {
+  handleSubmit() {
     // console.log (this.state);
     // socket.emit ('sendmsg', {text: this.state.text});
-    const from = this.props.user._id
-    const to = this.props.match.params.user
-    const msg = this.state.text
-    this.props.sendMsg({from,to,msg})
-    this.setState ({text: ''});
+    const from = this.props.user._id;
+    const to = this.props.match.params.user;
+    const msg = this.state.text;
+    this.props.sendMsg({ from, to, msg });
+    this.setState({ text: "" });
   }
-  render () {
+  render() {
     console.log(this.props);
-    const user = this.props.match.params.user
-    const Item = List.Item
+    const user = this.props.match.params.user;
+    const Item = List.Item;
     return (
-      <div id='chat-page'>
-        <NavBar mode='dark'>
-          {this.props.match.params.user}
-        </NavBar>
-        {this.props.chat.chatmsg.map (v => {
-          return v.from===user?(
+      <div id="chat-page">
+        <NavBar mode="dark">{this.props.match.params.user}</NavBar>
+        {this.props.chat.chatmsg.map(v => {
+          return v.from === user ? (
             <List key={v._id}>
               <Item>{v.content}</Item>
             </List>
-          ):(
-            <List className='chat-me' key={v._id}>
-              <Item extra={'avatar'}>{v.content}</Item>
+          ) : (
+            <List className="chat-me" key={v._id}>
+              <Item extra={"avatar"}>{v.content}</Item>
             </List>
-          )
+          );
         })}
         <div className="stick-footer">
           <List />
@@ -50,9 +51,9 @@ class Chat extends React.Component {
             placeholder="请输入"
             value={this.state.text}
             onChange={v => {
-              this.setState ({text: v});
+              this.setState({ text: v });
             }}
-            extra={<span onClick={() => this.handleSubmit ()}>发送</span>}
+            extra={<span onClick={() => this.handleSubmit()}>发送</span>}
           />
         </div>
       </div>
