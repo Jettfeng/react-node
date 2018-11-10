d0a89f54abfc416d7d65ca06bd18974cbcf853e3
1130034920@qq.com <1130034920@qq.com>
2018-11-10 11:58:34 +0800

    13-2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-<h2>单组件</h2>
+<h2>定制shouldComponentUpdate</h2>
+<p>react性能检测，在链接后面加上?react_perf,执行需要检测的操作，然后在浏览器的Performance里面查看</p>
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component,PureComponent } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,49 +6,78 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      num: 1
+      num: 1,
+      title: "imooc"
     };
-    console.log("构造函数执行");
     this.handleClick = this.handleClick.bind(this);
+    this.handleTitle = this.handleTitle.bind(this);
   }
   handleClick(e) {
-    console.log(e);
     this.setState({
       num: this.state.num + 1
     });
     console.log("xxxx");
   }
+  handleTitle() {
+    this.setState({
+      title: this.state.title + "!"
+    });
+  }
   render() {
-    console.log("render 执行中");
-    //每次渲染render，bind都会执行一次
-    const item = { react: "redux" };
     return (
       <div>
         <h2>
           App
           {this.state.num}
         </h2>
-        <button onClick={this.handleClick}>Btn0</button>
-        {/* <button onClick={this.handleClick.bind(this,'btn1')}>Btn1</button> */}
-        <button onClick={() => this.handleClick('btn2')}>Btn2</button>
-        <p>每一次都会生成新的对象传递</p>
-        {/* <Demo name={{ract:'redux'}}/> */}
-        {/* 推荐下面 这种写法 */}
-        <Demo name={item} />
+        <button onClick={this.handleClick}>BtnNum</button>
+        <button onClick={this.handleTitle}>BtnTitle</button>
+        <Demo title={this.state.title} />
       </div>
     );
   }
 }
-
-class Demo extends Component {
+// 方法一
+// class Demo extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   // 利用shouldComponentUpdate判断数据子组件接收的数据有没有变化，如果没变化，就不执行render
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log(nextProps);
+//     console.log(nextState);
+//     if (nextProps.title == this.props.title) {
+//       return false;
+//     }
+//     return true;
+//   }
+//   render() {
+//     console.log("demo render执行中");
+//     return (
+//       <div>
+//         <h2>
+//           I am Demo,
+//           {this.props.title}
+//         </h2>
+//       </div>
+//     );
+//   }
+// }
+// 方法二
+class Demo extends PureComponent {
   constructor(props) {
     super(props);
   }
+  // 利用PureComponente
+ 
   render() {
+    console.log("demo render执行中");
     return (
       <div>
-        <h2>I am Demo</h2>
-        <h2>{this.props.name.react}</h2>
+        <h2>
+          I am Demo,
+          {this.props.title}
+        </h2>
       </div>
     );
   }
